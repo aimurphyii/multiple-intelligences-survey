b46bccae7c18513a3d790c5e1594c4683b131761
Aileen Murphy <ai.murf2@gmail.com>
2019-03-16 17:30:19 -0700

    worked through js as a team

--- js/app.js
+++ js/app.js
@@ -7,17 +7,22 @@
 // create a form that asks for name, DOB, and date, as well as a button that submits teh form and starts the test.
 
 // For each question:
-// Have progress bar across top.
+// (mvp) have progress #/35 at top.
+// (stretch goal)Have progress bar across top.
 // (stretch goal) Have text for the question number with a speaker button next to it to have the question read to them if they want. 
 // Have text for the question.
 // Have true or false bottons for the user to click on.
 // (stretch goal) hover effects for true/false buttons.
+
 // Have event listener waiting for click.
 // Have event handler that will store answer (through counting catgeorized clicks?), 
+
 // add to total counter for progress bar, and pop up next question. Repeat.
 
 // Immediate results:
 // String letting user know what their highest area(s) of intelligence are. 
 // Pie chart of all areas.
+// (stretch goal)email link to self
+// (stretch goal) print friendly page
 // (stretch goal) Hyperlinks for each part of the pie chart linking to the area of intelligence on decsriptions page.
-// Button linking to more formal results page. 
+// (stretch goal) Button linking to more formal results page. 
